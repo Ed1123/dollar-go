f8545c9fa2c7ef67591f07f64691b30be63a1702
Ed1123 <44955050+Ed1123@users.noreply.github.com>
2025-08-04 13:16:37 -0500

    fix: add static files to server, fix import, rename functions

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"htmx-go-app/prices"
 	"net/http"
 	"text/template"
+
+	"github.com/Ed1123/dollar-go/prices"
 )
 
 func main() {
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/data", dataHandler)
+	http.HandleFunc("/prices", pricesHandler)
 
 	http.ListenAndServe(":8080", nil)
 }
@@ -22,13 +24,13 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, nil)
 }
 
-func dataHandler(w http.ResponseWriter, r *http.Request) {
+func pricesHandler(w http.ResponseWriter, r *http.Request) {
 	data, err := prices.GetPricesMock()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	tmpl, err := template.ParseFiles("templates/partials/data.html")
+	tmpl, err := template.ParseFiles("templates/data.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
