6f9449c3f69315a377310224e80191e84808689b
Ed1123 <44955050+Ed1123@users.noreply.github.com>
2025-08-20 00:56:24 -0500

    feat: add env and port env vars for testing and port

--- cmd/main.go
+++ cmd/main.go
@@ -4,16 +4,26 @@ import (
 	"html/template"
 	"log/slog"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/Ed1123/dollar-go/prices"
 )
 
+var IsTest bool
+
 func main() {
+	IsTest = strings.ToLower(os.Getenv("ENV")) == "dev"
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/prices", pricesHandler)
 
-	http.ListenAndServe(":8080", nil)
+	port, exists := os.LookupEnv("PORT")
+	if !exists {
+		port = "8080"
+	}
+	slog.Info("Server started", "url", "http://localhost:"+port, "test", IsTest)
+	http.ListenAndServe(":"+port, nil)
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -26,7 +36,12 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func pricesHandler(w http.ResponseWriter, r *http.Request) {
-	data, err := prices.GetPricesMock()
+	getPrices := prices.GetPrices
+	if IsTest {
+		slog.Info("Running in test mode, using mock prices")
+		getPrices = prices.GetPricesMock
+	}
+	data, err := getPrices()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
